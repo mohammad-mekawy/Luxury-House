f9cbfb64ae49260a7d64b65d9b2daac32ca07550
mohammad-mekawy <mohammad.mekawy92@gmail.com>
2022-01-20 09:06:37 +0200

    dist

--- form.php
+++ form.php
@@ -32,15 +32,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemail1'])){
 				$mail->CharSet = 'UTF-8';
 				$mail->SMTPDebug = 0;  
 				$mail->isSMTP();                            // Set mailer to use SMTP
-				$mail->Host = 'happenize.online';             // Specify main and backup SMTP servers
+				$mail->Host = 'royalapexts.com';             // Specify main and backup SMTP servers
 				$mail->SMTPAuth = true;                     // Enable SMTP authentication
-				$mail->Username = 'admin@happenize.online';          // SMTP username
-				$mail->Password = 'GszVA)pqRYws'; // SMTP password
+				$mail->Username = 'lp@royalapexts.com';          // SMTP username
+				$mail->Password = 'kiko123456KIKO'; // SMTP password
 				$mail->SMTPSecure = 'ssl';                            
 				$mail->Port = 465;  
 				$mail->isHTML(true);  // Set email format to HTML
 
-				$mail->setFrom('admin@happenize.online');
+				$mail->setFrom('lp@royalapexts.com');
 				$mail->addAddress($emails[$i]); 
 
 				//$mail->isHTML(true);  // Set email format to HTML
@@ -87,15 +87,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemail2'])){
 				$mail->CharSet = 'UTF-8';
 				$mail->SMTPDebug = 0;  
 				$mail->isSMTP();                            // Set mailer to use SMTP
-				$mail->Host = 'happenize.online';             // Specify main and backup SMTP servers
+				$mail->Host = 'royalapexts.com';             // Specify main and backup SMTP servers
 				$mail->SMTPAuth = true;                     // Enable SMTP authentication
-				$mail->Username = 'admin@happenize.online';          // SMTP username
-				$mail->Password = 'GszVA)pqRYws'; // SMTP password
+				$mail->Username = 'lp@royalapexts.com';          // SMTP username
+				$mail->Password = 'kiko123456KIKO'; // SMTP password
 				$mail->SMTPSecure = 'ssl';                            
 				$mail->Port = 465;  
 				$mail->isHTML(true);  // Set email format to HTML
 
-				$mail->setFrom('admin@happenize.online');
+				$mail->setFrom('lp@royalapexts.com');
 				$mail->addAddress($emails[$i]); 
 
 				//$mail->isHTML(true);  // Set email format to HTML
@@ -120,5 +120,248 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemail2'])){
 			}
     }
 }
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemail3'])){
+    $phone = filter_var($_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
 
+    $formErrors = array();
+	
+    if(empty($phone)){
+        $formErrors[] = "Add You phone please";
+    }
+    if(empty($formErrors)){
+            $emails = array('mohammad.mekawy92@gmail.com');
+			
+			for($i=0;$i<count($emails);$i++){
+				$mail = new PHPMailer;
+				$mail->CharSet = 'UTF-8';
+				$mail->SMTPDebug = 0;  
+				$mail->isSMTP();                            // Set mailer to use SMTP
+				$mail->Host = 'royalapexts.com';             // Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                     // Enable SMTP authentication
+				$mail->Username = 'lp@royalapexts.com';          // SMTP username
+				$mail->Password = 'kiko123456KIKO'; // SMTP password
+				$mail->SMTPSecure = 'ssl';                            
+				$mail->Port = 465;  
+				$mail->isHTML(true);  // Set email format to HTML
+
+				$mail->setFrom('lp@royalapexts.com');
+				$mail->addAddress($emails[$i]); 
+
+				//$mail->isHTML(true);  // Set email format to HTML
+
+               $bodyContent = '<h3 style="font-size:18px; margin-bottom:12px;">Phone: '. $phone .'</h3><br>';
+
+               $mail->Subject = 'Lead - Luxury House ';
+               $mail->Body    = $bodyContent;
+            //    $mail->send();
+               if(!$mail->Send()) {
+                    echo "Error while sending Email.";
+                    var_dump($mail);
+                } else {
+                    // echo "Email sent successfully";
+                    header("Location: thanks.html");
+                    // exit;
+                }
+                
+			}
+    }
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemailCallNow'])){
+    $phone = filter_var($_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
+
+    $formErrors = array();
+	
+    if(empty($phone)){
+        $formErrors[] = "Add You phone please";
+    }
+    if(empty($formErrors)){
+            $emails = array('mohammad.mekawy92@gmail.com');
+			
+			for($i=0;$i<count($emails);$i++){
+				$mail = new PHPMailer;
+				$mail->CharSet = 'UTF-8';
+				$mail->SMTPDebug = 0;  
+				$mail->isSMTP();                            // Set mailer to use SMTP
+				$mail->Host = 'royalapexts.com';             // Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                     // Enable SMTP authentication
+				$mail->Username = 'lp@royalapexts.com';          // SMTP username
+				$mail->Password = 'kiko123456KIKO'; // SMTP password
+				$mail->SMTPSecure = 'ssl';                            
+				$mail->Port = 465;  
+				$mail->isHTML(true);  // Set email format to HTML
+
+				$mail->setFrom('lp@royalapexts.com');
+				$mail->addAddress($emails[$i]); 
+
+				//$mail->isHTML(true);  // Set email format to HTML
+
+               $bodyContent = '<h3 style="font-size:18px; margin-bottom:12px;">Phone: '. $phone .'</h3><br>';
+
+               $mail->Subject = 'Lead - Luxury House ';
+               $mail->Body    = $bodyContent;
+            //    $mail->send();
+               if(!$mail->Send()) {
+                    echo "Error while sending Email.";
+                    var_dump($mail);
+                } else {
+                    // echo "Email sent successfully";
+                    header("Location: thanks.html");
+                    // exit;
+                }
+                
+			}
+    }
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemailCallLater'])){
+    $date  = filter_var($_POST['date'],FILTER_SANITIZE_STRING);
+    $time = filter_var($_POST['time'],FILTER_SANITIZE_STRING);
+    $phone = filter_var($_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
+
+    $formErrors = array();
+	
+    if(empty($date)){
+        $formErrors[] = "Choose date please";
+    }elseif(empty($phone)){
+        $formErrors[] = "Add You phone please";
+    }elseif(empty($time)){
+        $formErrors[] = "Choose time please";
+    }
+    if(empty($formErrors)){
+            $emails = array('mohammad.mekawy92@gmail.com');
+			
+			for($i=0;$i<count($emails);$i++){
+				$mail = new PHPMailer;
+				$mail->CharSet = 'UTF-8';
+				$mail->SMTPDebug = 0;  
+				$mail->isSMTP();                            // Set mailer to use SMTP
+				$mail->Host = 'royalapexts.com';             // Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                     // Enable SMTP authentication
+				$mail->Username = 'lp@royalapexts.com';          // SMTP username
+				$mail->Password = 'kiko123456KIKO'; // SMTP password
+				$mail->SMTPSecure = 'ssl';                            
+				$mail->Port = 465;  
+				$mail->isHTML(true);  // Set email format to HTML
+
+				$mail->setFrom('lp@royalapexts.com');
+				$mail->addAddress($emails[$i]); 
+
+				//$mail->isHTML(true);  // Set email format to HTML
+
+               $bodyContent = '<h3 style="font-size:18px; margin-bottom:12px;">Name: '. $date .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">Email: '. $time .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">Phone: '. $phone .'</h3><br>';
+
+               $mail->Subject = 'Lead - Luxury House ';
+               $mail->Body    = $bodyContent;
+            //    $mail->send();
+               if(!$mail->Send()) {
+                    echo "Error while sending Email.";
+                    var_dump($mail);
+                } else {
+                    // echo "Email sent successfully";
+                    header("Location: thanks.html");
+                    // exit;
+                }
+                
+			}
+    }
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemailLeaveMessage'])){
+    $email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+    $phone = filter_var($_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
+    $message  = filter_var($_POST['message'],FILTER_SANITIZE_STRING);
+
+    $formErrors = array();
+	
+    if(empty($phone)){
+        $formErrors[] = "Add You phone please";
+    }elseif(empty($email)){
+        $formErrors[] = "Add You email please";
+    }
+    if(empty($formErrors)){
+            $emails = array('mohammad.mekawy92@gmail.com');
+			
+			for($i=0;$i<count($emails);$i++){
+				$mail = new PHPMailer;
+				$mail->CharSet = 'UTF-8';
+				$mail->SMTPDebug = 0;  
+				$mail->isSMTP();                            // Set mailer to use SMTP
+				$mail->Host = 'royalapexts.com';             // Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                     // Enable SMTP authentication
+				$mail->Username = 'lp@royalapexts.com';          // SMTP username
+				$mail->Password = 'kiko123456KIKO'; // SMTP password
+				$mail->SMTPSecure = 'ssl';                            
+				$mail->Port = 465;  
+				$mail->isHTML(true);  // Set email format to HTML
+
+				$mail->setFrom('lp@royalapexts.com');
+				$mail->addAddress($emails[$i]); 
+
+				//$mail->isHTML(true);  // Set email format to HTML
+
+               $bodyContent = '<h3 style="font-size:18px; margin-bottom:12px;">Phone: '. $phone .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">Email: '. $email .'</h3><br>';
+               $bodyContent .= '<h3 style="font-size:18px; margin-bottom:12px;">Phone: '. $message .'</h3><br>';
+
+               $mail->Subject = 'Lead - Luxury House ';
+               $mail->Body    = $bodyContent;
+            //    $mail->send();
+               if(!$mail->Send()) {
+                    echo "Error while sending Email.";
+                    var_dump($mail);
+                } else {
+                    // echo "Email sent successfully";
+                    header("Location: thanks.html");
+                    // exit;
+                }
+                
+			}
+    }
+}
+if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sendemailQuickCall'])){
+    $phone = filter_var($_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
+
+    $formErrors = array();
+	
+    if(empty($phone)){
+        $formErrors[] = "Add You phone please";
+    }
+    if(empty($formErrors)){
+            $emails = array('mohammad.mekawy92@gmail.com');
+			
+			for($i=0;$i<count($emails);$i++){
+				$mail = new PHPMailer;
+				$mail->CharSet = 'UTF-8';
+				$mail->SMTPDebug = 0;  
+				$mail->isSMTP();                            // Set mailer to use SMTP
+				$mail->Host = 'royalapexts.com';             // Specify main and backup SMTP servers
+				$mail->SMTPAuth = true;                     // Enable SMTP authentication
+				$mail->Username = 'lp@royalapexts.com';          // SMTP username
+				$mail->Password = 'kiko123456KIKO'; // SMTP password
+				$mail->SMTPSecure = 'ssl';                            
+				$mail->Port = 465;  
+				$mail->isHTML(true);  // Set email format to HTML
+
+				$mail->setFrom('lp@royalapexts.com');
+				$mail->addAddress($emails[$i]); 
+
+				//$mail->isHTML(true);  // Set email format to HTML
+
+               $bodyContent = '<h3 style="font-size:18px; margin-bottom:12px;">Phone: '. $phone .'</h3><br>';
+
+               $mail->Subject = 'Lead - Luxury House ';
+               $mail->Body    = $bodyContent;
+            //    $mail->send();
+               if(!$mail->Send()) {
+                    echo "Error while sending Email.";
+                    var_dump($mail);
+                } else {
+                    // echo "Email sent successfully";
+                    header("Location: thanks.html");
+                    // exit;
+                }
+                
+			}
+    }
+}
 ?>
